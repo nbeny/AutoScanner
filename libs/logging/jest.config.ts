2feaf0bebf664693b6724dc7d52b87323a0c4d54
Nicolas BENY <ynebocin@gmail.com>
2026-05-25 11:06:07 +0200

    feat(logging): add Pino-based structured logging lib
    Adds @autoscanner/logging with buildPinoOptions factory (level, env,
    appName, pretty transport in dev, sensitive-field redaction) and
    AppLoggingModule wiring nestjs-pino via AppConfigService. Tags config
    and logging with scope:lib so @nx/enforce-module-boundaries allows the
    cross-lib dependency.

---
+++ libs/logging/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'logging',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  testMatch: ['**/*.spec.ts'],
+  coverageDirectory: '../../coverage/libs/logging',
+};
