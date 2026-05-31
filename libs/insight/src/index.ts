adebd33eb94a78ecf96529f5c5bd4d8b60fb8bf3
Nicolas BENY <ynebocin@gmail.com>
2026-05-31 15:55:51 +0200

    feat(insight): add getTopFindings (group by dedupHash)

--- libs/insight/src/index.ts
+++ libs/insight/src/index.ts
@@ -1,2 +1,4 @@
 export { getEngagementOverview } from './get-engagement-overview';
 export type { EngagementOverview, SeverityCounts } from './get-engagement-overview';
+export { getTopFindings } from './get-top-findings';
+export type { TopFinding } from './get-top-findings';
