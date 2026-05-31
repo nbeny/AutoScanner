46443c1a8747aee779b1f9b25c82095cbbfb7cef
Nicolas BENY <ynebocin@gmail.com>
2026-05-31 21:10:48 +0200

    feat(correlation): scaffold risk-score module

--- libs/correlation/src/index.ts
+++ libs/correlation/src/index.ts
@@ -4,3 +4,5 @@ export { findingDedupHash } from './finding-dedup';
 export type { FindingDedupHashInput } from './finding-dedup';
 export { AssetMergeService } from './asset-merge.service';
 export { CorrelationModule } from './correlation.module';
+export { computeRiskScore, SENSITIVE_PORTS, ADMIN_TOKENS } from './risk-score';
+export type { RiskScoreInput } from './risk-score';
